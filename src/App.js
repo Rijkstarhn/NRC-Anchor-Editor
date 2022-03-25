8a7e2dd6bd16c472c1a209e836c78e06e1db19cb
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2022-03-25 15:59:22 -0700

    develop branch created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
         <h1>Hello World</h1>
+        <h3>
+            Fancy display heading
+            <small className="text-muted">With faded secondary text</small>
+        </h3>
         <button type="button" class="btn btn-primary" onClick = {() => {alert("Clicked!")}}>Click Me!</button>
     </div>
   );
