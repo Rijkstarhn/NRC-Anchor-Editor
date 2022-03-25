8f6e11ce3fc049099788bb0561aa7e2d50e5aef5
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2022-03-25 15:48:17 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello World</h1>
+        <button type="button" class="btn btn-primary" onClick = {() => {alert("Clicked!")}}>Click Me!</button>
+    </div>
+  );
+}
+
+export default App;
