a5ba816814594efff737581318a77770495e8a2d
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2022-04-22 12:44:01 -0700

    Updated UI.

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -10,7 +10,7 @@ function Navbar() {
         <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
-                    <img src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" className="d-inline-block align-text-top"/>
+                    <img src="/images/bootstrap-logo.svg" alt="" width="30" height="24" className="d-inline-block align-text-top"/>
                         Anchor Editor
                 </a>
             </div>
